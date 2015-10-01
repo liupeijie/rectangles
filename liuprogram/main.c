606e7df18fc40f0162f26521c0087d1a36d113bc
liupeijie <liupeijie525@gmail.com>
2015-10-01 17:25:15 +0900

    Initial Commit

---
+++ liuprogram/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  liuprogram
+//
+//  Created by 向日葵会下雨 on H27/10/01.
+//  Copyright © 平成27年 向日葵会下雨. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
